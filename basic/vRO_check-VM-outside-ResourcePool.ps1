9f04e0263c8511cee0b1b8c4f5cd5728f9481d3f
Roberto <roberto.antoniello@studenti.unimi.it>
2024-03-14 09:38:09 +0100

    minor update

--- basic/vRO_check-VM-outside-ResourcePool.ps1
+++ basic/vRO_check-VM-outside-ResourcePool.ps1
@@ -15,13 +15,17 @@ function Handler($context, $inputs) {
     # Check VM on specific cluster
     $cluster = "<cluster-name>"
     $rp = Get-ResourcePool -Name Resources -Location $cluster
-    $vm1 =  Get-VM -Location $cluster | Where-Object {$_.ResourcePool.Id -eq $rp.Id -and !($_.Name -like "vCLS*")} | Select Name
+    $vm1 =  Get-VM -Location $cluster | Where-Object {$_.ResourcePool.Id -eq $rp.Id -and !($_.Name -like "vCLS*")}
     $result1 = ""
-    foreach($item in $vm1.Name){
-        $result1 += "$item`r`n"
+    foreach($item in $vm1){
+        if($($item.CustomFields.Item("vm.owner")) -ne ""){
+            $result1 += "$($item.Name) [proprietario] $($item.CustomFields.Item("vm.owner"))`r`n"
+        }else{
+            $result1 += "$($item.Name)`r`n"
+        }
     }
     #Notification via mail with a list of all VM outside resource pool
-    if($result1 -ne "" -and $result2 -ne ""){
+    if($result1 -ne "" -or $result2 -ne ""){
         send-MailMessage -To <receiver-1>,<receiver-2> -From <sender> -Subject "[Check VM] Workflow vRO " -Body "The following VMs are outside of a resource pool:`r`nCluster <cluster-name>:`r`n$($result1)`r`n -SmtpServer <ip-smtp-server> -Port 25
     }
 
