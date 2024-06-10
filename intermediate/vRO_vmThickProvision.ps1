4c42f440547c77b19752b6a681c77c485e2634d5
Roberto <roberto.antoniello@studenti.unimi.it>
2024-06-10 17:20:39 +0200

    update export csv e-mail

--- intermediate/vRO_vmThickProvision.ps1
+++ intermediate/vRO_vmThickProvision.ps1
@@ -13,21 +13,23 @@ function Handler($context, $inputs) {
 
     #check for a specific cluster 
     $vm1 = Get-VM -Location "<cluster-name>"
+    $pathcsv = "/tmp/vm-thick.csv"
     $vmThick1 = $vm1 | Get-HardDisk | where{$_.StorageFormat -eq 'Thick'} | Select @{N='VM';E={$_.Parent.Name}}, Name, StorageFormat, @{N='VMOwner';E={$_.Parent.CustomFields['vm.owner']}}, @{N='CapacityGB';E={$_.CapacityGB}}
-    $result1 = ""
+    $resultObject = @()
     $totalSpace1 = 0
     foreach($item in $vmThick1){
         $totalSpace1 += $($item.CapacityGB)
-        $owner = $item | Get-Annotation -Name 'vm.owner' | Select Value
-        if( $owner -ne ''){
-            $result1 += "`r`n$($item.VM) [occupied space] $($item.CapacityGB) GB [owner] $($item.VMOwner)`r`n"
-        }else{
-            $result1 += "`r`n$($item.VM) [occupied space] $($item.CapacityGB) GB`r`n"
+        #$owner = $item | Get-Annotation -Name 'vm.owner' | Select Value
+        $resultObject += [PSCustomObject]@{
+                    VM = $item.VM
+                    Proprietario = $item.VMOwner
+                    "Spazio Occupato GB" = $item.CapacityGB
         }
     }
+    $result1Object | Export-Csv -Path $pathcsv -NoTypeInformation -UseQuotes AsNeeded -Delimiter ';'
 
     if($totalSpace1 -ne 0 -or $totalSpace2 -ne 0){
-        send-MailMessage -To <receiver-1>,<receiver-2> -From <sender> -Subject "[Check VM] Workflow vRO " -Body "The following VMs has one or more disks with Thick Provision:`r`n<cluster-name>:`r`nOccupied space: $($totalSpace1) GB`r`n$($result1)`r`n" -SmtpServer <ip-server-smtp> -Port 25
+        send-MailMessage -To <receiver-1>,<receiver-2> -From <sender> -Subject "[Check VM] Workflow vRO " -Body "The following VMs has one or more disks with Thick Provision:`r`n<cluster-name>:`r`nOccupied space: $($totalSpace1) GB`r`n" -SmtpServer <ip-server-smtp> -Port 25 -Attachments $pathcsv
     }
     #Disconnect from vCenter
     Disconnect-VIserver -Server $inputs.vcenter -Force -Confirm:$false
